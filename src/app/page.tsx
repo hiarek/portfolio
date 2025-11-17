5150789c4cd0efbe14ff07b9eedc4e21a5ad297b
Arkadiusz Sroczyk <arkadiusz.s.student@gmail.com>
2025-11-17 21:32:00 +0100

    fix: initial animation fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
 							</figcaption>
 						</figure>
 
-						<h1 className='text-center max-w-[20ch] mx-auto text-balance sm:text-[40px] md:text-5xl leading-none text-[34px] font-semibold tracking-[-0.04em] animate-fade-in-delay'>
+						<h1 className='text-center max-w-[20ch] mx-auto text-balance sm:text-[40px] md:text-5xl leading-none text-[34px] font-semibold tracking-[-0.04em]'>
 							{/* I build and create things from scratch */}I like to build things from zero to one
 						</h1>
 						{/* <p className='text-white/80 text-balance max-w-[50ch] mx-auto text-center text-15 sm:text-base'>
